6558a648ef67bacc226581a9462ee36106001da9
Dmitri Pisarenko <dp@sw-dev.at>
2011-07-13 10:28:48 +0200

    Added interface ImmediateSchedulingRequest.

---
+++ src/main/java/co/altruix/pcc/api/cdm/ImmediateSchedulingRequest.java
@@ -0,0 +1,21 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010, 2011 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
+package co.altruix.pcc.api.cdm;
+
+/**
+ * @author DP118M
+ *
+ */
+public interface ImmediateSchedulingRequest {
+    Integer getUserId();
+    void setUserId(final Integer aUserId);
+}
