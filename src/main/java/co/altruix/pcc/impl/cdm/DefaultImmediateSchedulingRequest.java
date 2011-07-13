04c3c27da85c347ddc32bd4fdf223b8f6bab3dd4
Dmitri Pisarenko <dp@sw-dev.at>
2011-07-13 16:27:36 +0200

    Added class DefaultImmediateSchedulingRequest.

---
+++ src/main/java/co/altruix/pcc/impl/cdm/DefaultImmediateSchedulingRequest.java
@@ -0,0 +1,33 @@
+/**
+ * This file is part of Project Control Center (PCC).
+ * 
+ * PCC (Project Control Center) project is intellectual property of 
+ * Dmitri Anatol'evich Pisarenko.
+ * 
+ * Copyright 2010, 2011 Dmitri Anatol'evich Pisarenko
+ * All rights reserved
+ *
+ **/
+
+package co.altruix.pcc.impl.cdm;
+
+import co.altruix.pcc.api.cdm.ImmediateSchedulingRequest;
+
+/**
+ * @author DP118M
+ * 
+ */
+public final class DefaultImmediateSchedulingRequest implements
+        ImmediateSchedulingRequest {
+    private static final long serialVersionUID = 1L;
+    private Long userId;
+
+    public void setUserId(final Long aId) {
+        this.userId = aId;
+    }
+
+    public Long getUserId() {
+        return this.userId;
+    }
+
+}
